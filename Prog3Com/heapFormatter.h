a4d33f111910d3b630300dd236c081065599c0d6
Nathan <nathan.lea@okstate.edu>
2015-04-05 17:12:05 -0500

    More Clean Up

--- Prog3Com/heapFormatter.h
+++
@@ -1,8 +0,0 @@
-/****** FORMATTING BASED ON http://stackoverflow.com/questions/20922755/print-heap-array-in-tree-format ******/
-class heapFormatter {
-public:
-	size_t IntegerLogarithm2(size_t arg);
-	size_t IntegerPower2(size_t arg);
-	size_t LineLength(size_t level, size_t item_width, size_t spaces_between);
-	int formatHeap(int input [], int size);
-};
