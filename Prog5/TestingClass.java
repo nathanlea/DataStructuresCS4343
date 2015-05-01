710724afad71353a4816c4ef0dd0baffb56f4462
Nathan Lea <ngoalie@gmail.com>
2015-04-30 21:21:55 -0500

    Added Tests.

--- Prog5/TestingClass.java
+++ Prog5/TestingClass.java
@@ -7,6 +7,15 @@
 public class TestingClass
 {
     public TestingClass(){
+        String test1 = "0, 1, 2, 3, 4, 5";
+        String test2 = "0, 1, 2, 3";
+        String test3 = "0, 1";
+        String test4 = "5, 11, 10";
+        String test5 = "5, 11, 2";
+        String test6 = "7, 11, 2";
+        String verificaiton = "";
+
+        
         //Build the graph
         int s, t;
         Graph g = new Graph();
@@ -19,28 +28,70 @@ public class TestingClass
         s=0;t=5;
         String r = s+"";
         System.out.println("Longest Path from: " + s + " to "+  t);
-        System.out.println(g.longestPath(s,t, r));
+        verificaiton = g.longestPath(s,t, r);
+        System.out.print("Returned: " + verificaiton + " Expected: " + test1);
+        if(verificaiton.equals(test1)) {                
+            System.out.print("\tTest 1 PASSED!");
+        }else {
+            System.out.print("\tTest 1 FAILED!");
+        }
+        System.out.println();
         s=0;t=3; r = s+"";
         System.out.println("Longest Path from: " + s + " to "+  t);
-        System.out.println(g.longestPath(s,t, r));
+        verificaiton = g.longestPath(s,t, r);
+        System.out.print("Returned: " + verificaiton + " Expected: " + test2);
+        if(verificaiton.equals(test2)) {                
+            System.out.print("\tTest 2 PASSED!");
+        }else {
+            System.out.print("\tTest 2 FAILED!");
+        }
+        System.out.println();
         s=0;t=1; r = s+"";
         System.out.println("Longest Path from: " + s + " to "+  t);
-        System.out.println(g.longestPath(s,t, r));
+        verificaiton = g.longestPath(s,t, r);
+        System.out.print("Returned: " + verificaiton + " Expected: " + test3);
+        if(verificaiton.equals(test3)) {                
+            System.out.print("\tTest 3 PASSED!");
+        }else {
+            System.out.print("\tTest 3 FAILED!");
+        }
+        System.out.println();
 
         s = 5;t = 10;         
         Graph g6 = new Graph();        
         g6.addVertex(5,11); g6.addVertex(7,8);g6.addVertex(7,11);g6.addVertex(3,8);
         g6.addVertex(3,10);g6.addVertex(11,2);g6.addVertex(11,9);g6.addVertex(11,10);
         g6.addVertex(8,9);        
-        String r6 = s + " ";
+        String r6 = s + "";
         System.out.println("Longest Path from: " + s + " to "+  t);
-        System.out.println(g6.longestPath(s,t, r6));
-        s=5;t=2;r6 = s+" ";
+        verificaiton = (g6.longestPath(s,t, r6)).trim();
+        System.out.print("Returned: " + verificaiton + " Expected: " + test4);
+        if(verificaiton.equals(test4)) {                
+            System.out.print("\tTest 4 PASSED!");
+        }else {
+            System.out.print("\tTest 4 FAILED!");
+        }
+        System.out.println();
+        s=5;t=2;r6 = s+"";
         System.out.println("Longest Path from: " + s + " to "+  t);
-        System.out.println(g6.longestPath(s,t, r6));
-        s=7;t=2;r6 = s+" ";
+        verificaiton = g6.longestPath(s,t, r6);
+        System.out.print("Returned: " + verificaiton + " Expected: " + test5);
+        if(verificaiton.equals(test5)) {                
+            System.out.print("\tTest 5 PASSED!");
+        }else {
+            System.out.print("\tTest 5 FAILED!");
+        }
+        System.out.println();
+        s=7;t=2;r6 = s+"";
         System.out.println("Longest Path from: " + s + " to "+  t);
-        System.out.println(g6.longestPath(s,t, r6));
+        verificaiton = g6.longestPath(s,t, r6);
+        System.out.print("Returned: " + verificaiton + " Expected: " + test6);
+        if(verificaiton.equals(test6)) {                
+            System.out.print("\tTest 6 PASSED!");
+        }else {
+            System.out.print("\tTest 6 FAILED!");
+        }
+        System.out.println();
     }
 
     public static void Main(String[] args) {
